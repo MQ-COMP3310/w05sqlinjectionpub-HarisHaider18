e945ce51232d802f120b1cf55b1403cb6a209bb0
HarisHaider18 <126648232+HarisHaider18@users.noreply.github.com>
2025-03-28 10:44:52 +1100

    Last task remaing week 5

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -56,10 +56,15 @@ public class App {
             String line;
             int i = 1;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                if(line.matches("^[a-z]{4}$")){
+                System.out.println( line);
                 wordleDatabaseConnection.addValidWord(i, line);
-                i++;
             }
+                i++;
+        }
+    
+            
+        
 
         } catch (IOException e) {
             System.out.println("Not able to load . Sorry!");
@@ -74,13 +79,21 @@ public class App {
             String guess = scanner.nextLine();
 
             while (!guess.equals("q")) {
-                System.out.println("You've guessed '" + guess+"'.");
+                if(guess.matches("^[a-z]{4}$")){
+                    System.out.println("You've guessed '" + guess + "',");
+                
+
 
                 if (wordleDatabaseConnection.isValidWord(guess)) { 
                     System.out.println("Success! It is in the the list.\n");
+
                 }else{
                     System.out.println("Sorry. This word is NOT in the the list.\n");
                 }
+            } else{
+                System.out.print("the word '" + guess + "' is not valid." );
+
+            }
 
                 System.out.print("Enter a 4 letter word for a guess or q to quit: " );
                 guess = scanner.nextLine();
